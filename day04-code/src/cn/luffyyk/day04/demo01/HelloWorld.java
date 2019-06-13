60dcc9b05c7513ac804cc219871eb20d372c4156
yingkun1 <925712087@qq.com>
2019-06-13 22:13:08 +0800

    2019年6月13日22:13:06

---
+++ day04-code/src/cn/luffyyk/day04/demo01/HelloWorld.java
@@ -0,0 +1,20 @@
+package cn.luffyyk.day04.demo01;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    //    public static void main(String[] args) {
+//        System.out.println("Hello World!!!");
+//    }
+    public static void main(String[] args) {
+        System.out.println("你好！");
+        System.out.println("你好！");
+        System.out.println("你好！");
+        System.out.println("你好！");
+        System.out.println("你好！");
+        System.out.println("你好！");
+        System.out.println("你好！1");
+        System.out.println("你好！");
+    }
+}
+
---
+++ day05-code/src/cn/luffyyk/day05/demo/HelloJava.java
@@ -0,0 +1,7 @@
+package cn.luffyyk.day05.demo;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
